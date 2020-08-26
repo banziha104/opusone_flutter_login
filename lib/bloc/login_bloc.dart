2b174410b16dd0079ce368a7deb38018d8ec33fa
iyeongjun <banziha104@gmail.com>
2020-08-26 10:45:31 +0900

    ID PW 분할

--- lib/bloc/login_bloc.dart
+++ lib/bloc/login_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'login_event.dart';
@@ -5,11 +6,46 @@ import 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
 
+  List<Predicate> emailRule = [
+        (String email) => email.length > 10,
+        (String email) => email.indexOf('@') >= 0,
+  ];
+
+  List<Predicate> passwordRule = [
+        (String password) => password.length > 5,
+  ];
+
+  bool _checkRule(String text, List<Predicate> predicates) => predicates.where((predicate) => !predicate(text)).length == 0;
+
   @override
   LoginState get initialState => LoginState();
 
   @override
   Stream<LoginState> mapEventToState(LoginEvent event) async* {
-    yield LoginState.from(event);
+    var newState = LoginState.from(event);
+
+    bool isEmailValidated = _checkRule(event.email, emailRule);
+    bool isPasswordValidated = _checkRule(event.password, passwordRule);
+
+    if (event.uiEventType == UiEventType.EMAIL) {
+      if (isEmailValidated) {
+        newState.emailStatus = LoginStateStatus.CORRECT;
+      } else {
+        newState.emailStatus = LoginStateStatus.INCORRECT;
+      }
+    } else if (event.uiEventType == UiEventType.PASSWORD) {
+      if (isPasswordValidated) {
+        newState.passwordStatus = LoginStateStatus.CORRECT;
+      } else {
+        newState.passwordStatus = LoginStateStatus.INCORRECT;
+      }
+    }
+    debugPrint("${newState.emailStatus} ${newState.passwordStatus} |${newState.email}|${newState.password}|");
+
+    yield newState;
   }
+
+
 }
+
+enum LoginStateStatus { INIT, CORRECT, INCORRECT }
--- lib/bloc/login_state.dart
+++ lib/bloc/login_state.dart
@@ -10,14 +10,6 @@ class LoginState {
   String password = "";
   LoginStateStatus emailStatus = LoginStateStatus.INIT;
   LoginStateStatus passwordStatus = LoginStateStatus.INIT;
-  List<Predicate> emailRule = [
-    (String email) => email.length > 10,
-    (String email) => email.indexOf('@') >= 0,
-  ];
-
-  List<Predicate> passwordRule = [
-    (String password) => password.length > 5,
-  ];
 
   LoginState();
 
@@ -25,28 +17,5 @@ class LoginState {
       : this.email = loginEvent.email,
         this.password = loginEvent.password,
         this.emailStatus = loginEvent.oldState.emailStatus,
-        this.passwordStatus = loginEvent.oldState.passwordStatus {
-    bool isEmailValidated = _checkRule(email, emailRule);
-    bool isPasswordValidated = _checkRule(password, passwordRule);
-
-    if (loginEvent.uiEventType == UiEventType.EMAIL) {
-      if (isEmailValidated) {
-        this.emailStatus = LoginStateStatus.CORRECT;
-      } else {
-        this.emailStatus = LoginStateStatus.INCORRECT;
-      }
-    } else if (loginEvent.uiEventType == UiEventType.PASSWORD) {
-      if (isPasswordValidated) {
-        this.passwordStatus = LoginStateStatus.CORRECT;
-      } else {
-        this.passwordStatus = LoginStateStatus.INCORRECT;
-      }
-    }
-    debugPrint("$emailStatus $passwordStatus |$email|$password|");
-  }
-
-  bool _checkRule(String text, List<Predicate> predicates) =>
-      predicates.where((predicate) => !predicate(text)).length == 0;
+        this.passwordStatus = loginEvent.oldState.passwordStatus;
 }
-
-enum LoginStateStatus { INIT, CORRECT, INCORRECT }
